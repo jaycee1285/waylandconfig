20b5c9e07af69cb3b54d5b37f0f599f8409a89b7
jaycee1285 <curran.john.m@gmail.com>
2024-06-30 20:15:04 -0400

    From Nix

---
+++ fuzzel/fuzzel.nix
@@ -0,0 +1,26 @@
+[main]
+        font = "IosevkaTermSlab Nerd Font:weight=bold:size=14";
+        line-height = 25;
+        fields = "name,generic,comment,categories,filename,keywords";
+        terminal = "kitty";
+        prompt = "' ➜  '" ;
+        icon-theme = "Papirus-Nord";
+        layer = "top";
+        lines = 10;
+        width = 35;
+        horizontal-pad = 25;
+        inner-pad = 5;
+
+      [colors]
+        background = cdd3dd
+        text = 2E3440
+        match = EBCB8B
+        selection = 7b99b7
+        selection-match = 2E3440
+        selection-text = ECEFF4
+        border = 4C566A
+    
+      [border]
+        radius = 15
+        width = 3
+      
